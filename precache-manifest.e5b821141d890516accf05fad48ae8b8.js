fa86c1b60c0ada28049f8d603beab2bbbc7658db
oxyf <pnajimovich@gmail.com>
2021-09-13 18:36:01 -0300

    Updates

---
+++ precache-manifest.e5b821141d890516accf05fad48ae8b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05236b9e5ad795b9e80a4eed5cf48f52",
+    "url": "/react-three-fiber-demo/index.html"
+  },
+  {
+    "revision": "f83198181e6c0aa8b54f",
+    "url": "/react-three-fiber-demo/static/css/main.80583a33.chunk.css"
+  },
+  {
+    "revision": "2642e83bc28cd9385578",
+    "url": "/react-three-fiber-demo/static/js/2.782c8bbc.chunk.js"
+  },
+  {
+    "revision": "7d3596d7b85bfde798ac6194b99ebcf6",
+    "url": "/react-three-fiber-demo/static/js/2.782c8bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83198181e6c0aa8b54f",
+    "url": "/react-three-fiber-demo/static/js/main.781ec747.chunk.js"
+  },
+  {
+    "revision": "bb6e31a3dff6528fb5b3",
+    "url": "/react-three-fiber-demo/static/js/runtime-main.16721202.js"
+  }
+]);
